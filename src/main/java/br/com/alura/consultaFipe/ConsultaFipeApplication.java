53ccae8419acbacb94d96792ec6c817a507df1a8
Luiz-Gustavoo <gustavo_luiz45@hotmail.com>
2025-09-04 07:18:21 -0300

    Classe ConsultaFipeApplication.java

---
+++ src/main/java/br/com/alura/consultaFipe/ConsultaFipeApplication.java
@@ -0,0 +1,19 @@
+package br.com.alura.consultaFipe;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import br.com.alura.consultaFipe.Principal.Principal;
+
+@SpringBootApplication
+public class ConsultaFipeApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultaFipeApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Principal principal = new Principal();
+	}
+}
