a15e2d78000183fc3f4c7effa96bf9625873d391
Luiz-Gustavoo <gustavo_luiz45@hotmail.com>
2025-09-05 07:52:39 -0300

    Adicionar record marca

---
+++ src/main/java/br/com/alura/consultaFipe/Modelos/Marca.java
@@ -0,0 +1,10 @@
+package br.com.alura.consultaFipe.Modelos;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record Marca(@JsonAlias("codigo") String codigo,
+                    @JsonAlias("marca") String marca){
+
+}
