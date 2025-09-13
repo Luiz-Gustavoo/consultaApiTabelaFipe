83ab31f7a519013c34a46ae929560043d093cd07
Luiz-Gustavoo <gustavo_luiz45@hotmail.com>
2025-09-13 10:06:59 -0300

    Alterar o atributo de um array de Modelo para um list de Modelo

--- src/main/java/br/com/alura/consultaFipe/Modelos/Modelos.java
+++ src/main/java/br/com/alura/consultaFipe/Modelos/Modelos.java
@@ -3,6 +3,8 @@ package br.com.alura.consultaFipe.Modelos;
 import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import java.util.List;
+
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record Modelos(@JsonAlias ("modelos") Modelo[] modelos) {
+public record Modelos(@JsonAlias ("modelos") List<Modelo> modelos) {
 }
